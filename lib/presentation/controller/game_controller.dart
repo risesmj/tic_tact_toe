444c9d282f6c1e834349590f8599c6075d2b86a5
MJ <matheusjcunha@gmail.com>
2022-12-26 20:36:17 -0300

    Fix winner last line

--- lib/presentation/controller/game_controller.dart
+++ lib/presentation/controller/game_controller.dart
@@ -45,7 +45,7 @@ class GameController {
       //Horinzontais
       if ((board[0] == player && board[1] == player && board[2] == player) ||
           (board[3] == player && board[4] == player && board[5] == player) ||
-          (board[0] == player && board[1] == player && board[2] == player) ||
+          (board[6] == player && board[7] == player && board[8] == player) ||
 
           //Verticais
           (board[0] == player && board[3] == player && board[6] == player) ||
